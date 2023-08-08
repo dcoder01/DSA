c12b1f1c4927213691ff5e79d4ff18e0698bb31d
Debajyoti Debnath <115918228+dcoder01@users.noreply.github.com>
2023-08-08 19:42:15 +0530

    Create singleElement.cpp

---
+++ singleElement.cpp
@@ -0,0 +1,13 @@
+int getSingleElement(vector<int> &arr){
+	// Write your code here.
+	    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+
+	
+}
